8b0444b525c019a6dbfcb95140a2134370135916
sajkal <sajkal949@gmail.com>
2025-01-11 11:45:58 +0600

    finished about

--- src/components/site/pages/sections/about/welcome/AboutWelcome.tsx
+++ src/components/site/pages/sections/about/welcome/AboutWelcome.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import scss from "./AboutWelcome.module.scss";
+import raketa from "../../../../../../assets/Снимок экрана 2025-01-09 130716.png";
+import Image from "next/image";
+
 const AboutWelcome = () => {
   return (
     <section className={scss.AboutWelcome}>
       <div className="container">
-        <div className={scss.welcome_content}>AboutWelcome</div>
+        <div className={scss.welcome_content}>
+          <h1>
+            Мы являемся топливом для вашего бизнеса, готовы дать вам образование
+            и поднять ваш бренд до небес.
+          </h1>
+          <div className={scss.welcome_image}>
+            <Image src={raketa} alt="Raketa" className={scss.image_raketa} />
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/site/shared/join_us/JoinUs.tsx
+++ src/components/site/shared/join_us/JoinUs.tsx
@@ -1,10 +1,19 @@
-import React from "react";
 import scss from "./JoinUs.module.scss";
 const JoinUs = () => {
   return (
     <section className={scss.JoinUs}>
       <div className="container">
-        <div className={scss.welcome_content}>JoinUs</div>
+        <div className={scss.welcome_content}>
+          <h1>Присоединяйся к нам</h1>
+          <p>
+            Мы предоставляем множество функций, которые вы можете <br />
+            использовать. Постепенное накопление информация
+          </p>
+          <div className={scss.join_input}>
+            <input type="text" placeholder="Твой Email" />
+            <button>Подписка</button>
+          </div>
+        </div>
       </div>
     </section>
   );
